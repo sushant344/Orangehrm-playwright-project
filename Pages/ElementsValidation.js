9a8e01a94f7c8a03492932896339fede0cf03270
sushant344 <sushantpatil7208250133@gmail.com>
2024-04-30 20:07:09 +0530

    few changes

--- Pages/ElementsValidation.js
+++ Pages/ElementsValidation.js
@@ -3,47 +3,49 @@ export class ElementsValidation{
         this.page = page;
         this.expect = expect;
         this.mainmenulist = "//ul[@class='oxd-main-menu']//li";
+        this.usernamedropdown = "//img[@class='oxd-userdropdown-img']";
     }
 
     // check menu list --
     async checkmainmenuList(){
-        const list = await this.page.locator(this.mainmenulist).allTextContents();
-        this.expect(list).toBeTruthy();
-        const lists = await this.page.locator(this.mainmenulist).all();
-        this.expect(lists.length).toBe(12);
+      const list = await this.page.locator(this.mainmenulist).allTextContents();
+      this.expect(list).toBeTruthy();
+      const lists = await this.page.locator(this.mainmenulist).all();
+      this.expect(lists.length).toBe(12);
     }
 
     // check links --
     async checkLinks(){
-        const links = await this.page.$$("a");
-        for(let link of links){
-          this.expect(link.getAttribute("href")).toBeTruthy();
-        }
+      const links = await this.page.$$("a");
+      for(let link of links){
+        this.expect(link.getAttribute("href")).toBeTruthy();
+      }
     }
 
     // check images --
     async checkImages(){
-        const images = await this.page.$$("img");
-        for(let img of images){
-            this.expect(img.getAttribute("src")).toBeTruthy();
-        }
+      await this.page.locator(this.usernamedropdown).click();
+      const images = await this.page.$$("img");
+      for(let img of images){
+          this.expect(img.getAttribute("src")).toBeTruthy();
+      }
     }
 
     // check navigation is menu list --
     async checkNavigationofMenulist(){
-        const lists = await this.page.locator(this.mainmenulist).all();
-        for(let menulink of lists){
-          if(await menulink.textContent() !== "Maintenance"){
-            await menulink.click();
-            await this.checkLinks();
-            await this.checkImages();
-            await this.page.waitForTimeout(2000);
-          }
-          else{
-            await menulink.click();
-            await this.page.goBack();
-          }
+      const lists = await this.page.locator(this.mainmenulist).all();
+      for(let menulink of lists){
+        if(await menulink.textContent() !== "Maintenance"){
+          await menulink.click();
+          await this.checkLinks();
+          await this.checkImages();
+          await this.page.waitForTimeout(2000);
         }
+        else{
+          await menulink.click();
+          await this.page.goBack();
+        }
+      }
     }
 
 }
